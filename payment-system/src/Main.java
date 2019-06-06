166c967f3d74eae216f4e4affabe9f293973e697
Vitor Barcelos <vbc@ic.ufal.br>
2019-06-05 23:27:42 -0300

    Updates

--- payment-system/src/Main.java
+++ payment-system/src/Main.java
@@ -2,7 +2,6 @@ import java.util.Scanner;
 
 class Main {
 	public static Scanner input = new Scanner(System.in);
-
 	public static void initSystem(int maxPeople, String[] mPeople) {
 		for (int i = 0; i < maxPeople; i++) {
 			mPeople[i] = "";
@@ -38,16 +37,18 @@ class Main {
 		else if(idsEmployee[idEmployee] == 0) return 0;
 		else return 1;
 	}
-
 	public static void newEmployee(int maxPeople, String[] mEmployee, int[] idsEmployee, int[] idsSyndicate, String[][] undoEmployee, int[][] undoIdsEmployee, int[][] undoIdsSyndicate, int[] undoVerify, int[] redoVerify) {
 		int idEmployee;
 		String name = "";
 		String address = "";
 		String typeEmployee = "";
-		String paymentMethod = "";
+		String paymentSchedule = "";
+		String paymentMethod = "Bank";
+		int salary;
 		int syndicate = 0;
 		int idSyndicate = -1;
 		int syndicateRate = 0;
+		int ratePlus = 0;
 		int optionType = 0;
 		int confirm;
 
@@ -67,34 +68,35 @@ class Main {
 
 			if(optionType == 1) {
 				typeEmployee = "Commissioned";
-				paymentMethod = "bi-semanalmente";
+				paymentSchedule = "Bi-semanalmente";
 			} else if(optionType == 2) {
 				typeEmployee = "Salaried";
-				paymentMethod = "mensalmente";
+				paymentSchedule = "Mensalmente";
 			} else if(optionType == 3) {
 				typeEmployee = "Hourly";
-				paymentMethod = "semanalmente";
+				paymentSchedule = "Semanalmente";
 			} else optionType = 0;
 		} while (optionType == 0);
+
+		System.out.print("Salary: ");
+		salary = input.nextInt();
 
 		System.out.printf("\nID: %d\n", idEmployee);
 		System.out.printf("Name: %s\n", name);
 		System.out.printf("Address: %s\n", address);
 		System.out.printf("T. Employee: %s\n", typeEmployee);
+		System.out.printf("P. Schedule : %s\n", paymentSchedule);
 		System.out.printf("P. Method: %s\n", paymentMethod);
+		System.out.printf("Salary: %d\n", salary);
 		System.out.printf("Syndicate: %d\n", syndicate);
 		System.out.print("\nConfirm[1/0]: ");
 		confirm = input.nextInt();
 
 		if(confirm == 1) {
 			access(maxPeople, mEmployee, idsEmployee, idsSyndicate, undoEmployee, undoIdsEmployee, undoIdsSyndicate, undoVerify, redoVerify);
-			for(int i = 0; i < maxPeople; i++) {
-				redoVerify[i] = 0;
-			}
-
-			String Temp1 = String.format("<idEmployee>=%d<name>=%s<address>=%s", idEmployee, name, address);
-			String Temp2 = String.format("<typeEmployee>=%s<paymentMethod>=%s<syndicate>=%d", typeEmployee, paymentMethod, syndicate);
-			String Temp3 = String.format("<idSyndicate>=%d<syndicateRate>=%d<>", idSyndicate, syndicateRate);
+			String Temp1 = String.format("<idEmployee>=%d<name>=%s<address>=%s<typeEmployee>=%s", idEmployee, name, address, typeEmployee);
+			String Temp2 = String.format("<paymentSchedule>=%s<paymentMethod>=%s<salary>=%d", paymentSchedule, paymentMethod, salary);
+			String Temp3 = String.format("<syndicate>=%d<idSyndicate>=%d<syndicateRate>=%d<ratePlus>=%d<>", syndicate, idSyndicate, syndicateRate, ratePlus);
 			String employee = String.format("%s%s%s", Temp1, Temp2, Temp3);
 			mEmployee[idEmployee] = employee;
 			idsEmployee[idEmployee] = 1;
@@ -109,10 +111,13 @@ class Main {
 		String name = "";
 		String address = "";
 		String typeEmployee = "";
+		String paymentSchedule = "";
 		String paymentMethod = "";
+		String salary = "";
 		String syndicate = "";
 		String idSyndicate = "";
 		String syndicateRate = "";
+		String ratePlus = "";
 
 		char[] strAux1 = new char[256];
 		char tEquality = 0;
@@ -137,10 +142,13 @@ class Main {
 				if(tEquality == 1) name = strAux4;
 				if(tEquality == 2) address = strAux4;
 				if(tEquality == 3) typeEmployee = strAux4;
-				if(tEquality == 4) paymentMethod = strAux4;
-				if(tEquality == 5) syndicate = strAux4;
-				if(tEquality == 6) idSyndicate = strAux4;
-				if(tEquality == 7) syndicateRate = strAux4;
+				if(tEquality == 4) paymentSchedule = strAux4;
+				if(tEquality == 5) paymentMethod = strAux4;
+				if(tEquality == 6) salary = strAux4;
+				if(tEquality == 7) syndicate = strAux4;
+				if(tEquality == 8) idSyndicate = strAux4;
+				if(tEquality == 9) syndicateRate = strAux4;
+				if(tEquality == 10) ratePlus = strAux4;
 				tEquality += 1;
 				j = 0;
 			}
@@ -148,16 +156,19 @@ class Main {
 
 		do {
 			System.out.printf("Employee ID: %d\n", idEmployee);
-			if(syndicate.equals("1")) {
+			if(syndicate.charAt(0) == '1') {
 				System.out.printf("Syndicate ID: %s\n", idSyndicate);
 			}
 			System.out.printf("[2] - Name: %s\n", name);
 			System.out.printf("[3] - Address: %s\n", address);
 			System.out.printf("[4] - T. Employee: %s\n", typeEmployee);
-			System.out.printf("[5] - P. Method: %s\n", paymentMethod);
-			System.out.printf("[6] - Syndicate: %s\n", syndicate);
-			if(syndicate.equals("1")) {
-				System.out.printf("[7] - Syndicate Rate: %s\n", syndicateRate);
+			System.out.printf("[5] - P. Schedule: %s\n", paymentSchedule);
+			System.out.printf("[6] - P. Method: %s\n", paymentMethod);
+			System.out.printf("[7] - Salary: %s\n", salary);
+			System.out.printf("[8] - Syndicate: %s\n", syndicate);
+			if(syndicate.charAt(0) == '1') {
+				System.out.printf("[9] - Syndicate Rate: %s\n", syndicateRate);
+				System.out.printf("[10] - Syn. Rate Plus: %s\n", ratePlus);
 			}
 
 			System.out.println("\n[1] - Save Editing");
@@ -184,38 +195,58 @@ class Main {
 
 			}
 			if(menu == 5) {
+				System.out.print("\nP. Schedule: ");
+				paymentSchedule = input.nextLine();
+				paymentSchedule = input.nextLine();
+
+			}
+			if(menu == 6) {
 				System.out.print("\nP. Method: ");
 				paymentMethod = input.nextLine();
 				paymentMethod = input.nextLine();
 
 			}
-			if(menu == 6) {
-				System.out.print("\nSyndicate: ");
-				syndicate = input.nextLine();
-				syndicate = input.nextLine();
+			if(menu == 7) {
+				System.out.print("\nSalary: ");
+				salary = input.nextLine();
+				salary = input.nextLine();
 
-				if(syndicate.equals("1")) {
-					idSyndicate = getNewIdSyndicate(maxPeople, idsSyndicate);
-					syndicateRate = "25";
-				} else {
-					syndicate = "0";
-					syndicateRate = "0";
-					idSyndicate = "-1";
+			}
+			if(menu == 8) {
+				System.out.println("\nSyndicate: ");
+				if(syndicate.charAt(0) != '1') {
+					System.out.println("[1] - Enter");
+					System.out.println("[0] - Cancel");
+					System.out.print("Select option: ");
+					int option = input.nextInt();
+					if(option == 1) {
+						syndicate = "1";
+						idSyndicate = getNewIdSyndicate(maxPeople, idsSyndicate);
+						syndicateRate = "25";
+					}
+				}
+				else {
+					System.out.println("Only after one year");
 				}
 			}
-			if(menu == 7 && syndicate.equals("1")) {
+			if(menu == 9 && syndicate.charAt(0) == '1') {
 				System.out.print("\nSyndicate Rate: ");
 				syndicateRate = input.nextLine();
 				syndicateRate = input.nextLine();
 			}
+			if(menu == 10 && syndicate.charAt(0) == '1') {
+				System.out.print("\nSyn. Rate Plus: ");
+				ratePlus = input.nextLine();
+				ratePlus = input.nextLine();
+			}
 			if(menu == 1) {
 				access(maxPeople, mEmployee, idsEmployee, idsSyndicate, undoEmployee, undoIdsEmployee, undoIdsSyndicate, undoVerify, redoVerify);
-				String Temp1 = String.format("<idEmployee>=%d<name>=%s<address>=%s", idEmployee, name, address);
-				String Temp2 = String.format("<typeEmployee>=%s<paymentMethod>=%s<syndicate>=%s", typeEmployee, paymentMethod, syndicate);
-				String Temp3 = String.format("<idSyndicate>=%s<syndicateRate>=%s<>", idSyndicate, syndicateRate);
+				String Temp1 = String.format("<idEmployee>=%d<name>=%s<address>=%s<typeEmployee>=%s", idEmployee, name, address, typeEmployee);
+				String Temp2 = String.format("<paymentSchedule>=%s<paymentMethod>=%s<salary>=%s", paymentSchedule, paymentMethod, salary);
+				String Temp3 = String.format("<syndicate>=%s<idSyndicate>=%s<syndicateRate>=%s<ratePlus>=%s<>", syndicate, idSyndicate, syndicateRate, ratePlus);
 				String employee = String.format("%s%s%s", Temp1, Temp2, Temp3);
 				mEmployee[idEmployee] = employee;
-				if(syndicate.equals("1")) {
+				if(syndicate.charAt(0) == '1') {
 					int i = Integer.parseInt(idSyndicate);
 					idsSyndicate[i] = 1;
 				}
@@ -233,15 +264,97 @@ class Main {
 		String name = "";
 		String address = "";
 		String typeEmployee = "";
+		String paymentSchedule = "";
 		String paymentMethod = "";
+		String salary = "";
 		String syndicate = "";
 		String idSyndicate = "";
 		String syndicateRate = "";
+		String ratePlus = "";
 		int confirm;
 
 		char[] strAux1 = new char[256];
 		char tEquality = 0;
 		int menu;
+
+		if(verifyId(maxPeople, idsEmployee, idEmployee) != 1) {
+			System.out.println("Employee not found");
+			return;
+		}
+
+
+		int sizeEmployee = mEmployee[idEmployee].length();
+		for (int i = 0, j = 0; i < sizeEmployee; i++) {
+			if(mEmployee[idEmployee].charAt(i) == '=') {
+				i += 1;
+				while (mEmployee[idEmployee].charAt(i) != '<') {
+					strAux1[j++] = mEmployee[idEmployee].charAt(i++);
+				}
+				for (int k = j; k < strAux1.length; k++) strAux1[k] = ' ';
+				String strAux2 = String.copyValueOf(strAux1);
+				String strAux3 = strAux2.replaceAll("  ", "");
+				String strAux4 = strAux3.replaceAll(" <", "<");
+				if(tEquality == 1) name = strAux4;
+				if(tEquality == 2) address = strAux4;
+				if(tEquality == 3) typeEmployee = strAux4;
+				if(tEquality == 4) paymentSchedule = strAux4;
+				if(tEquality == 5) paymentMethod = strAux4;
+				if(tEquality == 6) salary = strAux4;
+				if(tEquality == 7) syndicate = strAux4;
+				if(tEquality == 8) idSyndicate = strAux4;
+				if(tEquality == 9) syndicateRate = strAux4;
+				if(tEquality == 10) ratePlus = strAux4;
+				tEquality += 1;
+				j = 0;
+			}
+		}
+
+		System.out.printf("\nID Employee: %d\n", idEmployee);
+		if(syndicate.charAt(0) == '1') {
+			System.out.printf("ID Syndicate: %s\n", idSyndicate);
+		}
+		System.out.printf("Name: %s\n", name);
+		System.out.printf("Address: %s\n", address);
+		System.out.printf("T. Employee: %s\n", typeEmployee);
+		System.out.printf("P. Schedule: %s\n", paymentSchedule);
+		System.out.printf("P. Method: %s\n", paymentMethod);
+		System.out.printf("Salary: %s\n", salary);
+		System.out.printf("Syndicate: %s\n", syndicate);
+		if(syndicate.charAt(0) == '1') {
+			System.out.printf("Syndicate Rate: %s\n", syndicateRate);
+			System.out.printf("Syn. Rate Plus: %s\n", ratePlus);
+		}
+		System.out.print("\nConfirm[1/0]: ");
+		confirm = input.nextInt();
+
+		if(confirm == 1) {
+			access(maxPeople, mEmployee, idsEmployee, idsSyndicate, undoEmployee, undoIdsEmployee, undoIdsSyndicate, undoVerify, redoVerify);
+			String employee = String.format("%s", "");
+			int i = Integer.parseInt(idSyndicate);
+			idsSyndicate[i] = 0;
+			idsEmployee[idEmployee] = 0;
+			mEmployee[idEmployee] = employee;
+			System.out.println("Sucess");
+		} else {
+			System.out.println("Canceled");
+		}
+	}
+	public static void insertFee(int maxPeople, String[] mEmployee, int[] idsEmployee, int[] idsSyndicate, String[][] undoEmployee, int[][] undoIdsEmployee, int[][] undoIdsSyndicate, int[] undoVerify, int[] redoVerify) {
+		System.out.print("\nEmployee ID: ");
+		int idEmployee = input.nextInt();
+		String name = "";
+		String address = "";
+		String typeEmployee = "";
+		String paymentSchedule = "";
+		String paymentMethod = "";
+		String salary = "";
+		String syndicate = "";
+		String idSyndicate = "";
+		String syndicateRate = "";
+		String ratePlus = "";
+
+		char[] strAux1 = new char[256];
+		char tEquality = 0;
 
 		if(verifyId(maxPeople, idsEmployee, idEmployee) != 1) {
 			System.out.println("Employee not found");
@@ -262,40 +375,50 @@ class Main {
 				if(tEquality == 1) name = strAux4;
 				if(tEquality == 2) address = strAux4;
 				if(tEquality == 3) typeEmployee = strAux4;
-				if(tEquality == 4) paymentMethod = strAux4;
-				if(tEquality == 5) syndicate = strAux4;
-				if(tEquality == 6) idSyndicate = strAux4;
-				if(tEquality == 7) syndicateRate = strAux4;
+				if(tEquality == 4) paymentSchedule = strAux4;
+				if(tEquality == 5) paymentMethod = strAux4;
+				if(tEquality == 6) salary = strAux4;
+				if(tEquality == 7) syndicate = strAux4;
+				if(tEquality == 8) idSyndicate = strAux4;
+				if(tEquality == 9) syndicateRate = strAux4;
+				if(tEquality == 10) ratePlus = strAux4;
 				tEquality += 1;
 				j = 0;
 			}
 		}
 
-		System.out.printf("\nID Employee: %d\n", idEmployee);
-		if(syndicate.equals("1")) {
-			System.out.printf("ID Syndicate: %s\n", idSyndicate);
+		System.out.printf("Employee ID: %d\n", idEmployee);
+		if(syndicate.charAt(0) == '1') {
+			System.out.printf("Syndicate ID: %s\n", idSyndicate);
 		}
 		System.out.printf("Name: %s\n", name);
 		System.out.printf("Address: %s\n", address);
 		System.out.printf("T. Employee: %s\n", typeEmployee);
+		System.out.printf("P. Schedule: %s\n", paymentSchedule);
 		System.out.printf("P. Method: %s\n", paymentMethod);
+		System.out.printf("Salary: %s\n", salary);
 		System.out.printf("Syndicate: %s\n", syndicate);
-		if(syndicate.equals("1")) {
+		if(syndicate.charAt(0) == '1') {
 			System.out.printf("Syndicate Rate: %s\n", syndicateRate);
+			System.out.printf("Syn. Rate Plus: %s\n", ratePlus);
 		}
-		System.out.print("\nConfirm[1/0]: ");
-		confirm = input.nextInt();
 
-		if(confirm == 1) {
-			access(maxPeople, mEmployee, idsEmployee, idsSyndicate, undoEmployee, undoIdsEmployee, undoIdsSyndicate, undoVerify, redoVerify);
-			String employee = String.format("%s", "");
-			mEmployee[idEmployee] = employee;
-			idsEmployee[idEmployee] = 0;
-			System.out.println("Sucess");
-		} else {
-			System.out.println("Canceled");
+		if(syndicate.charAt(0) != '1') {
+			System.out.println("The employee dos not participate in the syndicate");
+			return;
 		}
+
+		System.out.println("Insert New Rate: ");
+		int fee = input.nextInt();
+
+		access(maxPeople, mEmployee, idsEmployee, idsSyndicate, undoEmployee, undoIdsEmployee, undoIdsSyndicate, undoVerify, redoVerify);
+		String Temp1 = String.format("<idEmployee>=%d<name>=%s<address>=%s<typeEmployee>=%s", idEmployee, name, address, typeEmployee);
+		String Temp2 = String.format("<paymentSchedule>=%s<paymentMethod>=%s<salary>=%s", paymentSchedule, paymentMethod, salary);
+		String Temp3 = String.format("<syndicate>=%s<idSyndicate>=%s<syndicateRate>=%s<ratePlus>=%d<>", syndicate, idSyndicate, syndicateRate, fee);
+		String employee = String.format("%s%s%s", Temp1, Temp2, Temp3);
+		mEmployee[idEmployee] = employee;
 	}
+
 	public static void settings(int maxPeople, String[] mEmployee, int totalEmployees, String password) {
 		int menu;
 		do {
@@ -334,7 +457,6 @@ class Main {
 			}
 		}
 	}
-
 	public static void access(int maxPeople, String[] mEmployee, int[] idsEmployee, int[] idsSyndicate, String[][] undoEmployee, int[][] undoIdsEmployee, int[][] undoIdsSyndicate, int[] undoVerify, int[] redoVerify) {
 		newUndo(maxPeople, mEmployee, idsEmployee, idsSyndicate, undoEmployee, undoIdsEmployee, undoIdsSyndicate, undoVerify);
 		for(int i = 0; i < maxPeople; i++) {
@@ -410,18 +532,30 @@ class Main {
 		int people = input.nextInt();
 		int maxPeople = 10 * people;
 
-		System.out.print("Password:  ");
-		String password = input.nextLine();
-		password = input.nextLine();
+		System.out.print("Day: ");
+		String day = input.nextLine();
+		day = input.nextLine();
 
-		int totalEmployees;
-		int menu;
+		System.out.print("Month: ");
+		String month = input.nextLine();
+
+		System.out.print("Year: ");
+		String year = input.nextLine();
+
+		System.out.print("Password: ");
+		String password = input.nextLine();
+
+		int totalEmployees, menu;
+		int[][] calendar = new int[12][31];
+		int[] mensalmenteIds = new int[maxPeople];
+		int[] bisemanalmenteIds = new int[maxPeople];
+		int[] semanalmenteIds = new int[maxPeople];
 
 		String[] mEmployee = new String[maxPeople];
-		int[] idsEmployee = new int[maxPeople];
-		int[] idsSyndicate = new int[maxPeople];
 		String[][] undoEmployee = new String[maxPeople][maxPeople];
 		String[][] redoEmployee = new String[maxPeople][maxPeople];
+		int[] idsEmployee = new int[maxPeople];
+		int[] idsSyndicate = new int[maxPeople];
 		int[][] undoIdsEmployee = new int[maxPeople][maxPeople];
 		int[][] redoIdsEmployee = new int[maxPeople][maxPeople];
 		int[][] undoIdsSyndicate = new int[maxPeople][maxPeople];
@@ -466,6 +600,9 @@ class Main {
 			}
 			if(menu == 3) {
 				editEmployee(maxPeople, mEmployee, idsEmployee, idsSyndicate, undoEmployee, undoIdsEmployee, undoIdsSyndicate, undoVerify, redoVerify);
+			}
+			if(menu == 6) {
+				insertFee(maxPeople, mEmployee, idsEmployee, idsSyndicate, undoEmployee, undoIdsEmployee, undoIdsSyndicate, undoVerify, redoVerify);
 			}
 			if(menu == 8) {
 				undo(maxPeople, mEmployee, idsEmployee, idsSyndicate, undoEmployee, undoIdsEmployee, undoIdsSyndicate, redoEmployee, redoIdsEmployee, redoIdsSyndicate, undoVerify, redoVerify);
